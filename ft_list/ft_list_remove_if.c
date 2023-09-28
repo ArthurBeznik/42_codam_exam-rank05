8a9e2be8a77d3f13e1002e1907c69ccf96ff983b
abeznik <abeznik@student.codam.nl>
2023-09-28 16:26:07 +0200

    Added main

--- ft_list/ft_list_remove_if.c
+++ ft_list/ft_list_remove_if.c
@@ -15,11 +15,20 @@ cmp takes in two void *, and returns 0 if they are equal, 1 otherwise.
 */
 
 #include <stddef.h>
+#include <stdlib.h>
+
+#include <stdio.h>
 
 typedef struct s_list {
 	struct s_list	*next;
 	void			*data;
 }	t_list;
+
+int	cmp(void *data, void *ref) {
+	if (data == ref)
+		return 0;
+	return 1;
+}
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(void *, void *)) {
     t_list *current = *begin_list;
@@ -44,4 +53,44 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(void *, v
             current = current->next;
         }
     }
+}
+
+int	main(void) {
+	t_list *list = malloc(sizeof(t_list));
+
+	list->data = "1";
+	list->next = NULL;
+
+	t_list	*one = malloc(sizeof(t_list));
+	one->data = "2";
+	one->next = NULL;
+
+	t_list *two = malloc(sizeof(t_list));
+	two->data = "1";
+	two->next = NULL;
+
+	t_list *three = malloc(sizeof(t_list));
+	three->data = "3";
+	three->next = NULL;
+
+	list->next = one;
+	list->next->next = two;
+	list->next->next->next = three;
+
+	t_list *tmp = list;
+	printf("START\n");
+	while (tmp) {
+		printf("%s\n", tmp->data);
+		tmp = tmp->next;
+	}
+
+	ft_list_remove_if(&list, "1", *cmp);
+
+	printf("\nEND\n");
+	while (list) {
+		printf("%s\n", list->data);
+		list = list->next;
+	}
+
+	return 0;
 }
